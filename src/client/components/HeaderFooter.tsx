d426f33893349dd5372478a93b780978239c7b60
Martin Löffler <me@fatalerrorcoded.eu>
2019-06-01 00:12:45 +0200

    Social media icons

--- src/client/components/HeaderFooter.tsx
+++ src/client/components/HeaderFooter.tsx
@@ -12,7 +12,8 @@ export const FooterMenu: React.FunctionComponent<FooterMenuProps> = (props) => (
     <div className={css['footer-menu']}>
         <h3>{props.header}</h3>
         <ul>
-            {props.children && React.Children.map(props.children, (child) => <li>{child}</li>)}
+            {props.children && React.Children.map(props.children, (child, index) =>
+                <li key={index}>{child}</li>)}
         </ul>
     </div>
 );
@@ -25,9 +26,9 @@ export const Footer: React.FunctionComponent = (props) => (
                 <span className={css.copyright}>&copy;2019 Riot Communications</span>
             </div>
             <div className={css.social}>
-                <a href="https://facebook.com"><img src="assets/facebook.svg" title="Facebook" height="25px" /></a>
-                <a href="https://twitter.com/riotchat_"><img src="assets/twitter.svg" title="Twitter" height="25px" /></a>
-                <a href="https://instagram.com/riotchat"><img src="assets/instagram.svg" title="Instagram" height="25px" /></a>
+                <a href="https://facebook.com"><img src="/assets/images/social/facebook.svg" title="Facebook" height="25px" /></a>
+                <a href="https://twitter.com/riotchat_"><img src="/assets/images/social/twitter.svg" title="Twitter" height="25px" /></a>
+                <a href="https://instagram.com/riotchat"><img src="/assets/images/social/instagram.svg" title="Instagram" height="25px" /></a>
             </div>
             <FooterMenu header="Riot">
                 <a href="/download">Download</a>
