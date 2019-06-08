5921998e253c90bef0059f31296a012fd3d4ef5b
Paul Makles <paulmakles@gmail.com>
2019-06-08 21:01:51 +0100

    Create a global logging instance

--- src/server/main.ts
+++ src/server/main.ts
@@ -3,6 +3,7 @@ import * as path from 'path';
 
 import { getVersion } from '../shared/utils';
 import * as config from './config';
+import Logger from './system/logging';
 
 import apiRouter from './routes/api-router';
 import pagesRouter from './routes/pages-router';
@@ -10,7 +11,7 @@ import staticRouter from './routes/static-router';
 
 import { Helmet } from "react-helmet";
 
-console.log(`The App version is ${getVersion()}`);
+Logger.log(`Currently running RIOT version ${getVersion()}`);
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -21,5 +22,5 @@ app.use(staticRouter);
 app.use(pagesRouter);
 
 app.listen(config.SERVER_PORT, () => {
-  console.log(`App listening on port ${config.SERVER_PORT}!`);
+  Logger.info(`RIOT listening on :${config.SERVER_PORT}`);
 });
